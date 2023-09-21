f426cbbc0c4a7dff968521bc781bb09c8bf3bd74
Thomas Wang <77226475+peacebypieces@users.noreply.github.com>
2023-09-20 20:50:23 -0400

    Update MainActivity.java
    added functionality for exit button, added startbutton

--- app/src/main/java/com/example/cs2340a_team31/MainActivity.java
+++ app/src/main/java/com/example/cs2340a_team31/MainActivity.java
@@ -2,7 +2,9 @@ package com.example.cs2340a_team31;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +12,15 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.start_screen);
+
+        Button startButton = findViewById(R.id.startbutton);
+        Button exitButton = findViewById(R.id.exitbutton);
+
+        exitButton.setOnClickListener(v -> {
+            Intent exit = new Intent(Intent.ACTION_MAIN);
+            exit.addCategory(Intent.CATEGORY_HOME);
+            exit.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+            startActivity(exit);
+        });
     }
 }
