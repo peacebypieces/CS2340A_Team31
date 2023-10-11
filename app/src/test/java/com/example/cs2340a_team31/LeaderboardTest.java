9e3395e05f5d3671839fea19d2cdb61a9d3bf8f9
Thomas Wang <77226475+peacebypieces@users.noreply.github.com>
2023-10-11 19:36:00 -0400

    Create LeaderboardTest.java

---
+++ app/src/test/java/com/example/cs2340a_team31/LeaderboardTest.java
@@ -0,0 +1,17 @@
+package com.example.cs2340a_team31;
+
+import com.example.cs2340a_team31.model.LeaderBoard;
+
+import org.junit.Before;
+import org.junit.Test;
+
+public class LeaderboardTest {
+
+    @Before
+    public void setup(){
+        LeaderBoard testLB = LeaderBoard.getleaderboard();
+    }
+
+    @Test
+
+}
