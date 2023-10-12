7c8fd845761fe22e5bda68f1c60209514aec2fac
Thomas Wang <77226475+peacebypieces@users.noreply.github.com>
2023-10-11 20:25:51 -0400

    updated Unit Tests

--- app/src/test/java/com/example/cs2340a_team31/LeaderboardTest.java
+++ app/src/test/java/com/example/cs2340a_team31/LeaderboardTest.java
@@ -1,17 +1,43 @@
 package com.example.cs2340a_team31;
 
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotEquals;
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertNull;
+
 import com.example.cs2340a_team31.model.LeaderBoard;
+import com.example.cs2340a_team31.model.LeaderBoardPlayer;
 
 import org.junit.Before;
 import org.junit.Test;
 
 public class LeaderboardTest {
 
-    @Before
-    public void setup(){
-        LeaderBoard testLB = LeaderBoard.getleaderboard();
+    private LeaderBoard testLB;
+
+
+    // #11 Thomas Wang
+    @Test
+    public void testInitialization() {
+        testLB = LeaderBoard.getleaderboard();
+
+        assertNotNull(testLB.getPlayerSize());
     }
 
+    // #12 Thomas Wang
     @Test
+    public void testAddIntoLeaderBoard() {
+        testLB = LeaderBoard.getleaderboard();
+
+        LeaderBoardPlayer player = new LeaderBoardPlayer("Josh",
+                "11/10/2023 11:53PM",
+                200);
+
+        testLB.add(player);
+
+        assertEquals(1,testLB.getPlayerSize());
+        assertEquals(player,testLB.getPlayers()[0]);
+
+    }
 
 }
--- app/src/test/java/com/example/cs2340a_team31/PlayerTest.java
+++ app/src/test/java/com/example/cs2340a_team31/PlayerTest.java
@@ -6,10 +6,14 @@ import static org.junit.Assert.*;
 
 import com.example.cs2340a_team31.model.Player;
 
+import java.util.NoSuchElementException;
+
 public class PlayerTest {
 
     private Player player;
 
+
+    // #1 Shahbin Hossain
     @Test
     public void testInitialization() {
         player = Player.getPlayer();
@@ -19,23 +23,34 @@ public class PlayerTest {
         assertEquals(5.0,player.getMovementSpeed(),0);
     }
 
+    // #2 Shahbin Hossain
     @Test
-    public void testSetterAndGetterMethods() {
+    public void testPlayerXAndY() {
         player = Player.getPlayer();
 
         player.setX(5.0);
         assertEquals(5.0, player.getX(),0);
+        player.setX(0.0);
 
         player.setY(23.0);
         assertEquals(23.0,player.getY(),0);
+        player.setY(0.0);
 
         player.setScore(43);
         assertEquals(43,player.getScore(),0);
 
-        player.setName("IAmAName");
-        assertEquals("IAmAName",player.getName());
     }
 
+    // #3 Kenny Nguyen
+    @Test
+    public void testPlayerScore() {
+        player = Player.getPlayer();
+
+        player.setScore(43);
+        assertEquals(43,player.getScore(),0);
+    }
+
+    // #4 Kenny Nguyen
     @Test(expected = IllegalArgumentException.class)
     public void testEmptyPlayerName() {
         String name = "       ";
@@ -48,6 +63,7 @@ public class PlayerTest {
         player.setName(name);
     }
 
+    // #5 Joshua Johnson
     @Test
     public void testValidName() {
         String name = "Spaceman";
@@ -61,6 +77,65 @@ public class PlayerTest {
 
         // see if names are equal
         assertEquals(name, player.getName());
+    }
+
+    // #6 Joshua Johnson
+    @Test
+    public void testNameTrim() {
+        player = Player.getPlayer();
+
+        player.setName("         name");
+        assertEquals("name", player.getName());
+
+        player.setName("     name      ");
+        assertEquals("name", player.getName());
+
+        player.setName("name      ");
+        assertEquals("name", player.getName());
+
+        player.setName("     n    a    m    e      ");
+        assertEquals("name", player.getName());
+    }
+
+    // #7 Tran Ha
+    @Test
+    public void testDifficultyEasy() {
+        player = Player.getPlayer();
+
+        player.setDifficulty("Easy");
+
+        assertEquals(100.0, player.getHealth(),0);
+        assertEquals(100,player.getScore(),0);
+    }
+
+    // #8 Tran Ha
+    @Test
+    public void testDifficultyMedium() {
+        player = Player.getPlayer();
+
+        player.setDifficulty("Medium");
+
+        assertEquals(80.0, player.getHealth(),0);
+        assertEquals(200,player.getScore(),0);
+    }
+
+    // #9 Hoangyen Nguyen
+    @Test
+    public void testDifficultyHard() {
+        player = Player.getPlayer();
+
+        player.setDifficulty("Hard");
+
+        assertEquals(60.0, player.getHealth(),0);
+        assertEquals(300,player.getScore(),0);
+    }
+
+    // #10 Hoangyen Nguyen
+    @Test(expected = NoSuchElementException.class)
+    public void testNotGivenDifficulty() {
+        player = Player.getPlayer();
+
+        player.setDifficulty("this isn't a difficulty");
     }
 
 }
