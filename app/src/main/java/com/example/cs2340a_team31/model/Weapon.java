e8f02709d0f6ee2b7a805a8adb3cf0fce2abc635
Thomas Wang <77226475+peacebypieces@users.noreply.github.com>
2023-11-19 22:01:25 -0500

    created weapon and weaponview class

---
+++ app/src/main/java/com/example/cs2340a_team31/model/Weapon.java
@@ -0,0 +1,14 @@
+package com.example.cs2340a_team31.model;
+
+public class Weapon {
+    int damage;
+    String weapon_type;
+
+    public int getDamage() {
+        return damage;
+    }
+
+    public String getWeapon() {
+        return weapon_type;
+    }
+}
---
+++ app/src/main/java/com/example/cs2340a_team31/viewmodels/WeaponView.java
@@ -0,0 +1,32 @@
+package com.example.cs2340a_team31.viewmodels;
+
+
+import android.content.Context;
+
+import androidx.appcompat.widget.AppCompatImageView;
+
+import com.example.cs2340a_team31.model.observers.PlayerObserver;
+
+public class WeaponView extends AppCompatImageView {
+    private float x;
+    private float y;
+
+    public WeaponView(Context context) {
+        super(context);
+        this.x = 0;
+        this.y = 0;
+    }
+
+    public void set(float x, float y) {
+        this.setX(x);
+        this.setY(y);
+    }
+    public float getX() {
+        return x;
+    }
+
+    public float getY() {
+        return y;
+    }
+
+}
