f34d04704ccfbfbdbcc63d514094ca9b3c1d7df9
KennyNguyen4665 <hiddenbeggar@gmail.com>
2023-11-25 18:32:12 -0500

    sprint five unitTest2

--- app/src/test/java/com/example/cs2340a_team31/SprintFiveTest.java
+++ app/src/test/java/com/example/cs2340a_team31/SprintFiveTest.java
@@ -4,11 +4,14 @@ package com.example.cs2340a_team31;
 import static org.junit.Assert.assertEquals;
 
 import com.example.cs2340a_team31.model.Player;
+import com.example.cs2340a_team31.model.decorator.HealthPowerUp;
+import com.example.cs2340a_team31.model.decorator.PowerUp;
 import com.example.cs2340a_team31.model.enemyFactoryPattern.DawgEnemy;
 import com.example.cs2340a_team31.model.enemyFactoryPattern.*;
 
 import org.junit.Test;
 public class SprintFiveTest {
+    
 
     private Player player;
 
@@ -26,6 +29,18 @@ public class SprintFiveTest {
         assertEquals(100,player.getHealth(),0);
         assertEquals(1,player.getShield(),0);
         assertEquals(5,player.getAttackDamage(),0);
+
+    }
+    //#2 Kenny Nguyen
+    @Test
+    public void testInitializationHealthPowerUpDecorator() {
+        HealthPowerUp healthPow = new HealthPowerUp(player, 100,100,100);
+        player = Player.getPlayer();
+        player.setHealth(100);
+        assertEquals("HealthPowerUp", healthPow.getType());
+        assertEquals(100, healthPow.getBuff(),0);
+        assertEquals(100, healthPow.getWidth(),0);
+        assertEquals(100, healthPow.getHeight(),0);
 
     }
 
