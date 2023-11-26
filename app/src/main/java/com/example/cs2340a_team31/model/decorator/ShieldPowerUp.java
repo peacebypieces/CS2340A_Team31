4abb6cd93c2534c8db5480e30f9b933d45bc7a7c
WinterCherri <decaturhoangyen@gmail.com>
2023-11-26 13:10:04 -0500

    Shield power up doc

--- app/src/main/java/com/example/cs2340a_team31/model/decorator/ShieldPowerUp.java
+++ app/src/main/java/com/example/cs2340a_team31/model/decorator/ShieldPowerUp.java
@@ -3,7 +3,19 @@ package com.example.cs2340a_team31.model.decorator;
 import com.example.cs2340a_team31.model.Player;
 
 public class ShieldPowerUp extends PowerUp {
+    /**
+     * This class represents a Shield PowerUp in the game. It extends the PowerUp class.
+     * When applied, it increases the player's shield by a certain buff amount.
+     */
 
+    /**
+    * Constructs a new ShieldPowerUp object.
+    *
+    * @param player     The player to which the power-up is to be applied.
+    * @param buffAmount The amount by which the player's shield is to be increased.
+    * @param width      The width of the power-up.
+    * @param height     The height of the power-up.
+    */
     public ShieldPowerUp(Player player, double buffAmount, double width, double height) {
         setBuff(buffAmount);
         setSize(width, height);
@@ -12,6 +24,9 @@ public class ShieldPowerUp extends PowerUp {
         setStatus(false);
     }
 
+    /**
+     * Applies the power-up to the player. This increases the player's shield by the buff amount.
+     */
     @Override
     public void applyPowerUp() {
         double currentShield = getPlayer().getShield();
